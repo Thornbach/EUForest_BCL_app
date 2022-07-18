3d92a19b8fe0aa96b97797217883c819d4b58a52
Tobias Müller <ToMu94@outlook.de>
2022-07-18 16:02:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # EUForest_BCL_app
+
+ This app displays the distribution of European forest tree genera in combination with histograms designed to show the bioclimatic limits of each genus. (Work in Progress).
+ 
+ ## Original Data
+ 
+**Tree Distribution Data**: Mauri, Achille, Cescatti, Alessandro, GIRARDELLO, MARCO, Strona, Giovanni, Beck, Pieter, Caudullo, Giovanni, Manca, Federica, and Forzieri, Giovanni. 2022. “EU-Trees4F. A Dataset on the Future Distribution of European Tree Species.” https://doi.org/10.6084/M9.FIGSHARE.C.5525688.V1.
+
+**Bioclimatic Limits**: Karger, D.N., Conrad, O., Böhner, J., Kawohl, T., Kreft, H., Soria-Auza, R.W., Zimmermann, N.E., Linder, P., Kessler, M. (2017):  Climatologies at high resolution for the Earth land surface areas. **_Scientific Data_.** 4 170122. https://doi.org/10.1038/sdata.2017.122
+
+The Data was processed in QGIS and R.
+
+ 
+ 
---
+++ app.R
@@ -0,0 +1,108 @@
+#
+# Tobias Müller
+# ToMu94@outlook.de
+# SBIK-F Research Group: Biogeography and Ecosystem Ecology
+# 2022-07-18
+# 
+
+#-----------------------------------------------------------------------------#
+# Load in libraries
+#-----------------------------------------------------------------------------#
+
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+library(leaflet)
+
+#-----------------------------------------------------------------------------#
+# Load in Data and create list based on genus levels
+#-----------------------------------------------------------------------------#
+
+BioClimaticLimits <- read.csv("./data/FORESTGENERA_ALLDATA.csv")
+
+# change variable type
+
+BioClimaticLimits$Genus.name <- as.factor(BioClimaticLimits$Genus.name)
+BioClimaticLimits$GDD0 <- as.double(BioClimaticLimits$GDD0)
+BioClimaticLimits$GDD5 <- as.double(BioClimaticLimits$GDD5)
+BioClimaticLimits$GDD10 <- as.double(BioClimaticLimits$GDD10)
+
+# omit NA
+
+BioClimaticLimits <- na.omit(BioClimaticLimits)
+
+# Variables / 10
+
+BioClimaticLimits$TMAX <- BioClimaticLimits$TMAX/10
+BioClimaticLimits$TMIN <- BioClimaticLimits$TMIN/10
+BioClimaticLimits$GDD0 <- BioClimaticLimits$GDD0/10
+BioClimaticLimits$GDD5 <- BioClimaticLimits$GDD5/10
+BioClimaticLimits$GDD10 <- BioClimaticLimits$GDD10/10
+
+
+# Generate list of Genera for easier menu generation
+
+Genera <- levels(BioClimaticLimits$Genus.name)
+
+#-----------------------------------------------------------------------------#
+# UI
+#-----------------------------------------------------------------------------#
+
+ui <- bootstrapPage(
+  
+  # Application title
+  titlePanel("Bioclimatic limits for European Forest genera"),
+  
+  leafletOutput("genus_map"),
+  
+  absolutePanel(top = 10, right = 10,
+                pickerInput("Genus", label = "Select a Genus:",
+                            choices = list(`Genus` = Genera),
+                            options = list(`live-search` = TRUE)
+                )
+  ),
+  
+  sidebarPanel(
+    radioButtons("BCV", "Bioclimatic Variables",
+                 choices = list(`TMIN` = "TMIN",
+                                `TMAX` = "TMAX",
+                                `GDD0` = "GDD0",
+                                `GDD5` = "GDD5",
+                                `GDD10` = "GDD10")
+                                
+                )
+  ),
+  
+   mainPanel(
+     plotOutput(outputId = "histogram", height = "300px")
+   )
+)
+
+
+#-----------------------------------------------------------------------------#
+# Server Logic
+#-----------------------------------------------------------------------------#
+
+server <- function(input, output, session) {
+ 
+  filteredData <- reactive({
+      filter(BioClimaticLimits, Genus.name == input$Genus)
+  }) 
+  
+ output$histogram <- renderPlot({
+   color = "#434343"
+   hist(filteredData()[[input$BCV]])
+ })
+  
+  output$genus_map <- renderLeaflet({
+    leaflet(filteredData()) %>%
+      addProviderTiles(providers$Esri.WorldTopoMap) %>%
+      addCircleMarkers(~Lon, ~Lat,
+                       color = "Green",
+                       opacity = 0.5,
+                       radius = 0.1)
+ })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
